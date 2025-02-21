21a16f6af95acd3492d5cfe650716183cfc95007
percyashu <percyayukashu@gmail.com>
2025-02-21 19:28:20 +0100

    Initialisation

---
+++ src/main/java/io/greenshell/webwares/WebWaresApplication.java
@@ -0,0 +1,13 @@
+package io.greenshell.webwares;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebWaresApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WebWaresApplication.class, args);
+    }
+
+}
---
+++ src/test/java/io/greenshell/webwares/WebWaresApplicationTests.java
@@ -0,0 +1,13 @@
+package io.greenshell.webwares;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebWaresApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
